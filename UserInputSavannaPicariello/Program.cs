117aa3e857ae171b4845718d15122744a225e28b
Savanna Picariello <savannapicariello@gmail.com>
2020-01-29 16:49:39 -0500

    Add project files.

---
+++ UserInputSavannaPicariello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UserInputSavannaPicariello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, this is Savanna Picariello.");
+            Console.WriteLine("Please type something, then press enter...");
+
+            string response = Console.ReadLine();
+
+            Console.WriteLine("This is what you typed:");
+            Console.WriteLine(response);
+
+
+        }
+    }
+}
